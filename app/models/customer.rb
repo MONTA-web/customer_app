2e31ecd8dbbcf24653187df8fcfc0a59d174c164
MONTA-web <montamonta22@icoud.com>
2021-01-15 16:23:03 +0900

    customersテーブルバリデーション設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,16 @@ class Customer < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensios
   belongs_to :user
   belongs_to :prefecture
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 0 ,message: "Select"}
+    validates :city
+    validates :house_number
+    validates :visit_date
+  end
 end
