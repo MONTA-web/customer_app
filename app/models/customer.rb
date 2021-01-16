d480b5c07d08021a594a79b1e98afc770b496b71
MONTA-web <montamonta22@icoud.com>
2021-01-16 13:45:23 +0900

    エラーメッセージ編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,17 +1,20 @@
 class Customer < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensios
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :prefecture
 
   with_options presence: true do
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
-    validates :postal_code
-    validates :prefecture_id, numericality: { other_than: 0 ,message: "Select"}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/,message: "を全角ひらがな、全角カタカナ、漢字で入力してください"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角ひらがな、全角カタカナ、漢字で入力してください"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/,message: "を全角カタカナで入力してください"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/,message:"を全角カタカナで入力してください"}
+    validates :postal_code, format: {with:/\A\d{3}[-]\d{4}\z/,message: "をハイフン(-)含む7桁の半角数字を入力してください"}
+    validates :prefecture_id, numericality: { other_than: 0 ,message: "を選択してください"}
     validates :city
     validates :house_number
     validates :visit_date
   end
+
+  validates :amount_money, format: {with:/\A[0-9]+\z/,message: "を半角数字で入力してください"}, allow_blank: true
+   
 end
