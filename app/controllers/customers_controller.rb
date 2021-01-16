2f437a310a89496ce58a55fdfd9ea36850ed7921
MONTA-web <montamonta22@icoud.com>
2021-01-16 13:43:36 +0900

    コントローラー設定

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,20 @@
 class CustomersController < ApplicationController
-end
 
   def new
+    @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+        redirect_to root_path
+    else
+       render action: :new
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone,:product_name,:amount_money,:visit_date,:purchase_date,:remark_column).merge(user_id: current_user.id)
+  end
+end
