f99311f40104848ac39f21d123d365dee554fa7f
MONTA-web <montamonta22@icoud.com>
2021-01-18 16:17:51 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :customers, only:[:index,:new,:create]
+  resources :customers, only:[:index,:new,:create,:show]
 end
