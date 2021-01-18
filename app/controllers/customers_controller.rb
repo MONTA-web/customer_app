1c19a48072705947a2cd0984830e5ed17ef96cec
MONTA-web <montamonta22@icoud.com>
2021-01-18 12:16:14 +0900

    コントローラー設定

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!,only:[:new,:create]
+  before_action :authenticate_user!,only:[:index,:new,:create]
+
+  def index
+   @customers = current_user.customers.order("created_at DESC")
+  end
 
   def new
     @customer = Customer.new
@@ -18,4 +22,5 @@ class CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone,:product_name,:amount_money,:visit_date,:purchase_date,:remark_column).merge(user_id: current_user.id)
   end
+
 end
