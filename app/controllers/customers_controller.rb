dbfc8dab871d942c39cda882f939cbf96c75160d
MONTA-web <montamonta22@icoud.com>
2021-01-18 16:18:13 +0900

    コントローラー設定

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!,only:[:index,:new,:create]
+  before_action :authenticate_user!,only:[:index,:new,:create,:show]
+  before_action :redirect_to_home,only:[:show]
 
   def index
    @customers = current_user.customers.order("created_at DESC")
@@ -18,9 +19,20 @@ class CustomersController < ApplicationController
     end
   end
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone,:product_name,:amount_money,:visit_date,:purchase_date,:remark_column).merge(user_id: current_user.id)
+  end
+
+  def redirect_to_home
+    @customer = Customer.find(params[:id])
+    unless @customer.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
